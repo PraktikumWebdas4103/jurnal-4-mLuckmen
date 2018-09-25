ca990e7b42b167cd68b335addc56359d16b45c8c
Muhammad Luqman <43492565+mLuckmen@users.noreply.github.com>
2018-09-25 16:54:56 +0700

    Update formawal.php

--- formawal.php
+++ formawal.php
@@ -1,5 +1,11 @@
+<form action="proses.php" method="POST">
+	Username : <input type="text" name="username[]"><br>
+	Password : <input type="password" name="password[]"><br>
+	<input type="submit" name="submit1" value="submit">
+</form>
 <?php
-
-
-
+	if (isset($_POST['submit1'])) {
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+	}
 ?>
